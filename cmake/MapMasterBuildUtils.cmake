a9c1fdacb16c700abae3785c9f1bdcc68e4e8079
GasparVardanyan <gaspar_pm@proton.me>
2026-08-08 17:09:46 +0400

    separated cmake build utils

---
+++ cmake/MapMasterBuildUtils.cmake
@@ -0,0 +1,18 @@
+function (enableReleaseBuildOptimizations target)
+	target_compile_options (${target} PRIVATE
+		$<$<CONFIG:Release>:-O3>
+		$<$<CONFIG:Release>:-march=native>
+		$<$<CONFIG:Release>:-mtune=native>
+		$<$<CONFIG:Release>:-flto>
+	)
+
+	target_link_options (${target} PRIVATE
+		$<$<CONFIG:Release>:-flto>
+	)
+endfunction ()
+
+function (enableDebugBuildDebugInfos target)
+	target_compile_options(${target} PRIVATE
+		$<$<CONFIG:Debug>:-g -fno-omit-frame-pointer>
+	)
+endfunction ()
