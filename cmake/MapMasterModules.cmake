9aa1b4f61f20b9bd7cec299052dd192f9189fbac
GasparVardanyan <gaspar_pm@proton.me>
2026-08-24 23:18:29 +0400

    backup

--- cmake/MapMasterModules.cmake
+++ cmake/MapMasterModules.cmake
@@ -2,18 +2,6 @@ add_subdirectory (
 	${CMAKE_CURRENT_LIST_DIR}/../modules/Tanki
 	${CMAKE_CURRENT_BINARY_DIR}/MapMaster_Module_Tanki
 )
-add_subdirectory (
-	${CMAKE_CURRENT_LIST_DIR}/../modules/TankiCore
-	${CMAKE_CURRENT_BINARY_DIR}/MapMaster_Module_TankiCore
-)
-add_subdirectory (
-	${CMAKE_CURRENT_LIST_DIR}/../modules/TankiRaylibBackend
-	${CMAKE_CURRENT_BINARY_DIR}/MapMaster_Module_TankiRaylibBackend
-)
-add_subdirectory (
-	${CMAKE_CURRENT_LIST_DIR}/../modules/TankiR3DBackend
-	${CMAKE_CURRENT_BINARY_DIR}/MapMaster_Module_TankiR3DBackend
-)
 
 add_subdirectory (
 	${CMAKE_CURRENT_LIST_DIR}/../modules/Scene3D
