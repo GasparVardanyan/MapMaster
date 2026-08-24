1a6063f1a47c3cb3a8a231b3a12dc78aff64938a
GasparVardanyan <gaspar_pm@proton.me>
2026-08-24 23:35:10 +0400

    cleanup

--- modules/Tanki/modules/TankiCore/src/PropCPUResourceManager.cpp
+++
@@ -1,6 +0,0 @@
-# include "MapMaster/Tanki/PropCPUResourceManager.hpp"
-# include "MapMaster/Tanki/PropCPUResourceManagerR3DBackend.hpp"
-# include "MapMaster/Tanki/PropCPUResourceManagerRaylibBackend.hpp"
-
-# include "MapMaster/Tanki/PropCPUResourceManager.inl" // IWYU pragma: keep
-
